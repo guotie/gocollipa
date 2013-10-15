df70ee5b205d4bc0d97cc5b6f993d1462d60b297
guotie <guotie.9@gmail.com>
2013-10-15 22:38:25 +0800

    bash modify from studygolang

---
+++ getpkg.bat
@@ -0,0 +1,26 @@
+@echo off
+
+setlocal
+
+if exist getpkg.bat goto ok
+echo getpkg.bat must be run from its folder
+goto end
+
+:ok
+
+set OLDGOPATH=%GOPATH%
+if not exist ..\thirdparty mkdir ..\thirdparty
+set GOPATH=%~dp0..\thirdparty
+
+go get -u github.com/go-sql-driver/mysql
+go get -u github.com/gorilla/mux
+go get -u github.com/gorilla/sessions
+go get -u github.com/astaxie/beego/cache
+
+:: hg clone https://code.google.com/p/go.net src/go.net
+:: go install go.net/websocket 
+
+set GOPATH=%OLDGOPATH%
+
+:end
+echo finished
---
+++ install.bat
@@ -0,0 +1,28 @@
+@echo off
+
+setlocal
+
+if exist install.bat goto ok
+echo install.bat must be run from its folder
+goto end
+
+:ok
+
+set OLDGOPATH=%GOPATH%
+set GOPATH=%GOPATH%;%~dp0
+
+if not exist log mkdir log
+
+go get -u github.com/go-sql-driver/mysql
+go get -u github.com/gorilla/mux
+go get -u github.com/gorilla/securecookie
+
+gofmt -w src
+
+:: -tags "debug" 表示测试
+go install -tags "debug" gocollipa
+
+set GOPATH=%OLDGOPATH%
+
+:end
+echo finished
